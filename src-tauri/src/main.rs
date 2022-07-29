f8710a3df7ad2dca7c6c13386f7da8335bfa7880
Gregory Albouy <60700958+GregoryAlbouy@users.noreply.github.com>
2022-07-29 11:06:00 +0200

    feat: bootstrap Tauri app (#3)
    * Initialize project using Create React App
    
    * Bootstrap Tauri app

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,21 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+use tauri::Manager;
+
+fn main() {
+  tauri::Builder::default()
+    .setup(|app| {
+      #[cfg(debug_assertions)] // only include this code on debug builds
+      {
+        let window = app.get_window("main").unwrap();
+        window.open_devtools();
+        window.close_devtools();
+      }
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
