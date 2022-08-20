015af93c8444f465f84f86041acb19e851e02edc
moreirathomas <moreirathomas97@gmail.com>
2022-08-20 17:01:21 +0200

    feat: handle sidecar manually in web mode

---
+++ scripts/sidecar-exec.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Identify the platform.
+target_triple=$(rustc -Vv | grep host | cut -f2 -d' ')
+
+# The binary is suffixed with the triple target as required by tauri sidecar api.
+program="benchttp-server-$target_triple"
+
+./src-tauri/bin/$program
--- src/hooks/useSpawnEngine.ts
+++ src/hooks/useSpawnEngine.ts
@@ -8,7 +8,17 @@ export function useSpawnEngine() {
   useEffect(() => {
     const spawn = async () => {
       setIsLoading(true)
-      await spawnEngine()
+
+      try {
+        await spawnEngine()
+      } catch (error) {
+        if (!isWeb()) throw error
+        console.warn(
+          `Running in the browser: cannot spawn sidecar with @tauri-apps/api/shell. Make sure engine is running:
+                npm run sidecar:exec`
+        )
+      }
+
       setIsLoading(false)
     }
     spawn()
@@ -16,3 +26,8 @@ export function useSpawnEngine() {
 
   return { isLoading }
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type WindowTauri = typeof window & { __TAURI__: any }
+
+const isWeb = () => (window as WindowTauri).__TAURI__ === undefined
