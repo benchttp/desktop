87f97b5e1c99547a0cc80575d811e54db9fc0095
moreirathomas <moreirathomas97@gmail.com>
2022-08-21 21:59:33 +0200

    chore: rename spwanAsync to spawnSync
    It actually is a wrapper to allow synchronous call.

--- src/hooks/useSpawnEngine.ts
+++ src/hooks/useSpawnEngine.ts
@@ -7,7 +7,7 @@ import { useOnce } from './useOnce'
 export function useSpawnEngine() {
   const [isLoading, setIsLoading] = useState(false)
 
-  const spawnAsync = () => {
+  const spawnSync = () => {
     const spawn = async () => {
       setIsLoading(true)
 
@@ -29,7 +29,7 @@ export function useSpawnEngine() {
   const once = useOnce()
 
   useEffect(() => {
-    once.do(spawnAsync)
+    once.do(spawnSync)
   }, [once])
 
   return { isLoading }
