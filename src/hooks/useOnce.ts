efc995f9a024f7f4f94ca600aab87825d94c145f
moreirathomas <moreirathomas97@gmail.com>
2022-08-21 21:58:17 +0200

    chore: return Destructor from useOnce.do

--- src/hooks/useOnce.ts
+++ src/hooks/useOnce.ts
@@ -4,11 +4,10 @@ export function useOnce() {
   const done = useRef(false)
 
   const once = {
-    done,
     do: (effect: React.EffectCallback) => {
       if (done.current) return
-      effect()
       done.current = true
+      return effect()
     },
   }
 
