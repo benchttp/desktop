be1888b16b3690c104c9eba4d48f47ab66916aed
alex-mngt <mongeot.alex@gmail.com>
2022-08-31 00:12:55 +0200

    feat : setup router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
-// eslint-disable-next-line import/default
-import React from 'react'
-// eslint-disable-next-line import/default
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
-import App from './App'
-import './index.css'
+import { Router } from './router'
+import './style/index.scss'
 
-// eslint-disable-next-line import/no-named-as-default-member
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </StrictMode>
 )
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals()
