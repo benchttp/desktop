339d145ce739ec255ce5a591e4aa6e6e0966c463
C <>
2022-09-08 15:35:50 +0200

    feat: add tag component

---
+++ src/components/Tag/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+
+import { getClassNames } from './core/tag.helpers'
+import { IProps } from './core/tag.typings'
+
+export const Tag: FC<IProps> = ({ text, color='white', className }) => {
+    const classNames = getClassNames({ color, className })
+  
+    return (
+      <div className={classNames.join(' ')}>
+        {text}
+      </div>
+    )
+  }
