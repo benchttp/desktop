0e12b636c51ae704e375ec4b0d9c7f63499b5ceb
moreirathomas <moreirathomas97@gmail.com>
2022-09-08 14:19:41 +0200

    fix: fix Provider undefined in test

--- src/pages/Home/index.test.tsx
+++ src/pages/Home/index.test.tsx
@@ -1,11 +1,18 @@
 import { render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux'
 import { describe, expect, test } from 'vitest'
+
+import { store } from '@/store'
 
 import { Home } from './index'
 
 describe('Home', () => {
   test('renders the App component', () => {
-    render(<Home />)
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    )
     expect(screen.getByText(/Benchttp/i)).toBeDefined()
   })
 })
