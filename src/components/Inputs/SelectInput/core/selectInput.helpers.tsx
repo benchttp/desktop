6a7abc1ba935ea1864330993c794ab970b234b32
alex-mngt <mongeot.alex@gmail.com>
2022-09-18 20:10:12 +0200

    feat : select input component

---
+++ src/components/Inputs/SelectInput/core/selectInput.helpers.tsx
@@ -0,0 +1,13 @@
+import { IProps } from './selectInput.typings'
+
+export const getClassName = ({
+  className,
+}: Pick<IProps, 'className'>): string[] => {
+  const classNames: string[] = ['f', 'f-direction-column']
+
+  if (className) {
+    classNames.push(className)
+  }
+
+  return classNames
+}
