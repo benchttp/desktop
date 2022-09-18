bde5d772275e26c60d7bc21f023940dbae14644e
C <>
2022-09-18 16:09:53 +0200

    Create tag component

---
+++ src/components/Tag/core/tag.helpers.tsx
@@ -0,0 +1,21 @@
+import s from './tag.module.scss'
+import { IProps } from './tag.typings'
+
+export const getClassNames = ({
+  color,
+  className,
+}: Pick<IProps, 'className'> & Pick<Required<IProps>, 'color'>) => {
+  const classNames: string[] = [
+    s['tag'],
+    s[`tag-${color}`],
+    'f',
+    'f-direction-row',
+    'f-ai-center',
+  ]
+
+  if (className) {
+    classNames.push(className)
+  }
+
+  return classNames
+}
