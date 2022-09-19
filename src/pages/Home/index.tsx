1fc0bf5822e3c4f04f4144ff28aea7d4c0ddf5dd
Damien MATHIEU <damien.mathieu612@gmail.com>
2022-09-19 14:49:00 +0200

    feat: Removed from index page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,6 @@
 import { FC } from 'react'
-import { CheckCircle, PieChart, Settings } from 'react-feather'
 
 import { Typography } from '@/components'
-import { Tab } from '@/components/Tab'
 import { SimpleStream } from '@/examples'
 
 export const Home: FC = () => {
@@ -10,9 +8,6 @@ export const Home: FC = () => {
     <>
       <Typography element="h1">Benchttp</Typography>
       <SimpleStream />
-      <Tab link="#" color="primary" text="Configure" iconStart={Settings} />
-      <Tab link="#" color="white" text="Test results" iconStart={CheckCircle} />
-      <Tab link="#" color="white" text="Summary" iconStart={PieChart} />
     </>
   )
 }
