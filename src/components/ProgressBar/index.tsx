bac97f45b0ce39ed501110b692a010e122395d42
nartydev <narty.dev@gmail.com>
2022-09-30 00:12:20 +0200

    chore: fix progressbar

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -8,7 +8,7 @@ export const ProgressBar: FC<IProps> = ({ max, value }) => {
   const percentage = (value: number, max: number) => (100 * value) / max
   return (
     <div className={s['container']}>
-      <Typography>
+      <Typography className={s['percentage']}>
         {value}/{max} requests ({percentage(value, max)}%)
       </Typography>
       <progress className={s['progress-bar']} max={max} value={value} />
