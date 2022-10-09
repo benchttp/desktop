cb8da6e6ca9608a38b16aef2413eebc0c7c10077
c1-ra <clara.gonnon@gmail.com>
2022-10-09 18:45:14 +0200

    Add StatCard to ResultDisplay

--- src/views/Run/internal/ResultDisplay/ResultDisplay.tsx
+++ src/views/Run/internal/ResultDisplay/ResultDisplay.tsx
@@ -1,7 +1,16 @@
-import { StopCircle } from 'react-feather'
+import {
+  BarChart2,
+  CheckCircle,
+  ChevronsDown,
+  ChevronsUp,
+  Clock,
+  StopCircle,
+  Target,
+} from 'react-feather'
 
 import { RunProgress, RunReport } from '@/benchttp'
 import { Button, ProgressBar, Typography } from '@/components'
+import { StatCard } from './StatCard'
 
 interface IProps {
   progress: RunProgress | null
@@ -22,6 +31,7 @@ export const ResultDisplay: React.FC<IProps> = ({
       <ProgressSection {...progress} stop={stop} />
     )}
     {report && <ReportSection {...report} />}
+    {report && <ResultsSection {...report} />}
     {error && <ErrorSection error={error} />}
   </div>
 )
@@ -51,9 +61,71 @@ const ProgressSection: React.FC<RunProgress & { stop: () => false | void }> = ({
 const ReportSection: React.FC<RunReport> = ({ metrics }) => (
   <section>
     <Typography element="h3">Report</Typography>
+    <div></div>
     <div>Min: {formatDuration(metrics.responseTimes.min)}</div>
     <div>Max: {formatDuration(metrics.responseTimes.max)}</div>
     <div>Avg: {formatDuration(metrics.responseTimes.mean)}</div>
+  </section>
+)
+
+const ResultsSection: React.FC<RunReport> = ({ metrics }) => (
+  <section>
+    <Typography element="h3">Results</Typography>
+    <div></div>
+    <div className="f f-direction-row f-ai-end mb-3">
+      <StatCard
+        icon={ChevronsUp}
+        iconColor="blue"
+        className="mr-3"
+        stat={formatDuration(metrics.responseTimes.min)}
+        label="fastest response"
+      />
+      <StatCard
+        icon={ChevronsDown}
+        iconColor="orange"
+        className="mr-3"
+        stat={formatDuration(metrics.responseTimes.max)}
+        label="slowest response"
+      />
+      <StatCard
+        icon={BarChart2}
+        iconColor="base-white"
+        className="mr-3"
+        stat={formatDuration(metrics.responseTimes.mean)}
+        label="average response time"
+      />
+    </div>
+
+    <div className="f f-direction-row f-ai-end mb-3">
+      {metrics.responseTimes.deciles && metrics.responseTimes.deciles[8] ? (
+        <StatCard
+          icon={Clock}
+          iconColor="primary"
+          className="mr-3"
+          stat={formatDuration(metrics.responseTimes.deciles[8])}
+          label="90% of requests are faster"
+        />
+      ) : (
+        <div></div>
+      )}
+      <StatCard
+        icon={Target}
+        iconColor="purple"
+        className="mr-3"
+        stat={formatDuration(metrics.responseTimes.standardDeviation)}
+        label="standard deviation"
+      />
+      <StatCard
+        icon={CheckCircle}
+        iconColor="get"
+        className="mr-3"
+        stat={formatSuccesfulRequestsNumber(
+          metrics.records.length,
+          metrics.requestFailures.length
+        )}
+        label="of requests were successful"
+      />
+    </div>
   </section>
 )
 
@@ -67,4 +139,19 @@ const ErrorSection: React.FC<{ error: string }> = ({ error }) => (
 function formatDuration(ns: number): string {
   const ms = ns / 1_000_000
   return `${ms.toFixed(2)}ms`
+}
+
+function calculateSuccesfulRequestsPercentage(
+  recordsNb: number,
+  failuresNb: number
+): number {
+  return ((recordsNb - failuresNb) * 100) / recordsNb
+}
+
+function formatSuccesfulRequestsNumber(
+  recordsNb: number,
+  failuresNb: number
+): string {
+  const percentage = calculateSuccesfulRequestsPercentage(recordsNb, failuresNb)
+  return `${percentage.toFixed(2)}%`
 }
