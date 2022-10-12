d8ec6047b429daddd7a0f303bc64d38154d474b2
c1-ra <clara.gonnon@gmail.com>
2022-10-12 12:44:07 +0200

    Remove useless comments

--- src/views/Run/internal/ResultDisplay/internal/ChartsSection/internal/BarChart/internal/BarChart.helpers.tsx
+++ src/views/Run/internal/ResultDisplay/internal/ChartsSection/internal/BarChart/internal/BarChart.helpers.tsx
@@ -2,13 +2,7 @@ import s from './bar-chart.module.scss'
 import { IProps } from './BarChart.types'
 
 export const getClassNames = ({ className }: Pick<IProps, 'className'>) => {
-  const classNames: string[] = [
-    s['barChart'],
-    // 'f',
-    // 'f-direction-row',
-    // 'f-ai-center',
-    // 'p-3',
-  ]
+  const classNames: string[] = [s['barChart']]
 
   if (className) {
     classNames.push(className)
