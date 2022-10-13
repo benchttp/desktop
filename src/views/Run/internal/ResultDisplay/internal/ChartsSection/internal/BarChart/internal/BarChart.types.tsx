45fbcbf27f7e326a37f8d9a4f431b1945a09e5b1
c1-ra <clara.gonnon@gmail.com>
2022-10-13 08:58:38 +0200

    Refacto ChartsSection.helpers

--- src/views/Run/internal/ResultDisplay/internal/ChartsSection/internal/BarChart/internal/BarChart.types.tsx
+++ src/views/Run/internal/ResultDisplay/internal/ChartsSection/internal/BarChart/internal/BarChart.types.tsx
@@ -1,6 +1,11 @@
-export interface IProps {
+export interface IBarChartEntry {
   className?: string
   data: { xAxis: string; yAxis: number }[]
   xLegend: string
   yLegend: string
+}
+
+export interface IBarChartData {
+  xAxis: string
+  yAxis: number
 }
--- src/views/Run/internal/ResultDisplay/internal/ChartsSection/internal/ChartsSection.helpers.ts
+++ src/views/Run/internal/ResultDisplay/internal/ChartsSection/internal/ChartsSection.helpers.ts
@@ -1,43 +1,62 @@
 import { RunReport } from '@/benchttp'
 import { nanosecondsToMilliseconds } from '@/tools/converters'
 
+import { IBarChartData } from './BarChart/internal/BarChart.types'
+
 export const getResponseTimeDistribution = (
   responseTimes: { responseTime: number }[],
-  maxTime: RunReport['metrics']['responseTimes']['max']
-): { xAxis: string; yAxis: number }[] => {
+  max: RunReport['metrics']['responseTimes']['max']
+): IBarChartData[] => {
   const partitions = 10
-  const responseTimeDistribution = [...Array(partitions)].map((_, i) => ({
-    timeRangeMax: (maxTime / partitions) * (i + 1),
+
+  return computeDistribution({ responseTimes, partitions, max }).map(
+    formatEntry
+  )
+}
+
+interface IComputeDistribution {
+  responseTimes: { responseTime: number }[]
+  partitions: number
+  max: number
+}
+
+interface IDistribution {
+  timeRangeMax: number
+  count: number
+}
+
+const computeDistribution = ({
+  responseTimes,
+  partitions,
+  max,
+}: IComputeDistribution): IDistribution[] => {
+  const distribution = [...Array(partitions)].map((_, i) => ({
+    timeRangeMax: (max / partitions) * (i + 1),
     count: 0,
   }))
 
-  for (let i = 0; i < responseTimes.length; i++) {
-    for (let j = 0; j < partitions; j++) {
-      if (
-        responseTimes[i]['responseTime'] >
-        responseTimeDistribution[j]['timeRangeMax']
-      ) {
-        continue
-      } else {
-        responseTimeDistribution[j]['count'] += 1
+  responseTimes.forEach(({ responseTime }) => {
+    for (let i = 0; i < partitions; i++) {
+      if (responseTime == max) {
+        if (responseTime <= distribution[i].timeRangeMax) {
+          distribution[i].count += 1
+          break
+        }
+      }
+      if (responseTime < distribution[i].timeRangeMax) {
+        distribution[i].count += 1
         break
       }
     }
-  }
+  })
 
-  const responseTimeDistributionFormatted: {
-    xAxis: string
-    yAxis: number
-  }[] = []
-
-  for (let i = 0; i < partitions; i++) {
-    responseTimeDistributionFormatted[i] = {
-      xAxis: nanosecondsToMilliseconds(
-        responseTimeDistribution[i]['timeRangeMax']
-      ).toFixed(2),
-      yAxis: responseTimeDistribution[i]['count'],
-    }
-  }
-
-  return responseTimeDistributionFormatted
+  return distribution
 }
+
+const formatEntry = ({
+  timeRangeMax,
+  count,
+}: IDistribution): IBarChartData => ({
+  xAxis: nanosecondsToMilliseconds(timeRangeMax).toFixed(2),
+  yAxis: count,
+})
