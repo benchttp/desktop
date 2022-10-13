b4937ac4335aee225ed3aadd219189839c02e070
c1-ra <clara.gonnon@gmail.com>
2022-10-13 10:44:53 +0200

    Refacto ChartsSection.helpers

--- src/views/Run/internal/ResultDisplay/internal/ChartsSection/internal/ChartsSection.helpers.ts
+++ src/views/Run/internal/ResultDisplay/internal/ChartsSection/internal/ChartsSection.helpers.ts
@@ -1,62 +1,52 @@
-import { RunReport } from '@/benchttp'
 import { nanosecondsToMilliseconds } from '@/tools/converters'
 
 import { IBarChartData } from './BarChart/internal/BarChart.types'
 
+const partitions = 10
+
 export const getResponseTimeDistribution = (
   responseTimes: { responseTime: number }[],
-  max: RunReport['metrics']['responseTimes']['max']
-): IBarChartData[] => {
-  const partitions = 10
-
-  return computeDistribution({ responseTimes, partitions, max }).map(
-    formatEntry
-  )
-}
-
-interface IComputeDistribution {
-  responseTimes: { responseTime: number }[]
-  partitions: number
   max: number
+): IBarChartData[] => {
+  return computeDistribution(responseTimes, partitions, max).map(formatEntry)
 }
 
-interface IDistribution {
+interface Bar {
   timeRangeMax: number
   count: number
 }
 
-const computeDistribution = ({
-  responseTimes,
-  partitions,
-  max,
-}: IComputeDistribution): IDistribution[] => {
-  const distribution = [...Array(partitions)].map((_, i) => ({
+const computeDistribution = (
+  responseTimes: { responseTime: number }[],
+  partitions: number,
+  max: number
+): Bar[] => {
+  const distribution: Bar[] = [...Array(partitions)].map((_, i) => ({
     timeRangeMax: (max / partitions) * (i + 1),
     count: 0,
   }))
 
   responseTimes.forEach(({ responseTime }) => {
-    for (let i = 0; i < partitions; i++) {
-      if (responseTime == max) {
-        if (responseTime <= distribution[i].timeRangeMax) {
-          distribution[i].count += 1
-          break
-        }
-      }
-      if (responseTime < distribution[i].timeRangeMax) {
-        distribution[i].count += 1
-        break
-      }
+    const index = findRank(responseTime, distribution)
+    if (index !== -1) {
+      distribution[index].count++
     }
+    // Silently ignore values that are out of range
   })
 
   return distribution
 }
 
-const formatEntry = ({
-  timeRangeMax,
-  count,
-}: IDistribution): IBarChartData => ({
+/**
+ * Finds the index of the bar that the given value belongs to.
+ * Expects the bars to be sorted by their limits ascending.
+ * Returns null if the value is out of range.
+ */
+const findRank = (value: number, distribution: Bar[]): number => {
+  return distribution.findIndex((bar) => value <= bar.timeRangeMax)
+}
+
+const formatEntry = ({ timeRangeMax, count }: Bar): IBarChartData => ({
   xAxis: nanosecondsToMilliseconds(timeRangeMax).toFixed(2),
   yAxis: count,
 })
