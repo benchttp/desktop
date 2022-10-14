c2ce9125aca32336e8687adc6af4dbf730f5902d
alex-mngt <mongeot.alex@gmail.com>
2022-10-14 14:22:56 +0200

    fix : add benchttp logo

--- src/views/Run/Run.tsx
+++ src/views/Run/Run.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react'
-import { CheckCircle, PieChart, Settings } from 'react-feather'
+import { CheckCircle, PieChart, Settings, Zap } from 'react-feather'
 import { Navigate, Route, Routes, useNavigate } from 'react-router-dom'
 
-import { Tab } from '@/components'
+import { Tab, Typography } from '@/components'
 
 import { Configuration, ResultDisplay, Summary } from './internal/components'
 import {
@@ -21,31 +21,39 @@ export const Run: FC = () => {
 
   return (
     <>
-      <div className="f f-ai-center mb-4">
-        <Tab
-          className="mr-3"
-          text="Configure"
-          link="configure"
-          iconStart={Settings}
-        />
-        <Tab
-          className="mr-3"
-          text="Tests results"
-          link="tests-results"
-          iconStart={CheckCircle}
-          disabled={isTestResultsDisabled({
-            report,
-            progress,
-            error,
-            appError,
-          })}
-        />
-        <Tab
-          text="Summary"
-          link="summary"
-          iconStart={PieChart}
-          disabled={isSummaryDisabled({ report, progress, error, appError })}
-        />
+      <div className="f f-ai-center f-jc-space-b mb-4">
+        <div className="f f-ai-center">
+          <Tab
+            className="mr-3"
+            text="Configure"
+            link="configure"
+            iconStart={Settings}
+          />
+          <Tab
+            className="mr-3"
+            text="Tests results"
+            link="tests-results"
+            iconStart={CheckCircle}
+            disabled={isTestResultsDisabled({
+              report,
+              progress,
+              error,
+              appError,
+            })}
+          />
+          <Tab
+            text="Summary"
+            link="summary"
+            iconStart={PieChart}
+            disabled={isSummaryDisabled({ report, progress, error, appError })}
+          />
+        </div>
+        <div className="f f-ai-center">
+          <Zap color="#f1b445" className="mr-1" />
+          <Typography font="poppins" size="h4" weight="bold" color="primary">
+            Benchttp
+          </Typography>
+        </div>
       </div>
 
       <Routes>
