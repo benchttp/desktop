19010f83b1bf77af3a98cf79b6935f5b8657c3e1
Gregory Albouy <60700958+GregoryAlbouy@users.noreply.github.com>
2022-10-14 12:47:41 +0200

    fix: negative timeout (#97)
    * fix: apply threshold to rendered timeout
    
    * refactor: rename withThreshold -> applyThreshold
    
    * fix: restore timeout label

--- src/views/Run/internal/ResultDisplay/ResultDisplay.tsx
+++ src/views/Run/internal/ResultDisplay/ResultDisplay.tsx
@@ -2,6 +2,7 @@ import { StopCircle } from 'react-feather'
 
 import { RunError, RunProgress, RunReport } from '@/benchttp'
 import { AppError, Button, ProgressBar, Typography } from '@/components'
+import { nanosecondsToSeconds, applyThreshold } from '@/tools/converters'
 
 import { RunErrorDisplay } from '../components'
 import { ReportSection } from './internal/components'
@@ -51,7 +52,8 @@ const ProgressSection: React.FC<RunProgress & { stop: () => false | void }> = ({
     <div>
       <ProgressBar max={maxCount} value={doneCount} />
       <Typography className="mt-2 mb-5">
-        Timeout in: {((timeout - elapsed) / 1_000_000_000).toFixed(0)}s
+        Timeout in:{' '}
+        {applyThreshold(0)(nanosecondsToSeconds(timeout - elapsed)).toFixed(0)}s
       </Typography>
     </div>
   </section>
