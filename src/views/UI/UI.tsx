f9a7fd99ecbb3391119d9bb9382a6b23c42a6b58
moreirathomas <moreirathomas97@gmail.com>
2022-10-14 11:01:09 +0200

    chore: integrate in UI

--- src/views/UI/UI.tsx
+++ src/views/UI/UI.tsx
@@ -562,6 +562,10 @@ export const UI: FC = () => {
           <Tooltip icon={AlertCircle} className="mr-3">
             Tooltip text
           </Tooltip>
+          <Tooltip icon={AlertCircle} className="mr-3">
+            <a href="#">Go top</a>
+            <p>blabla</p>
+          </Tooltip>
         </div>
       </div>
       <div className="mb-4">
